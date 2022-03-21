7f5c5469a9084938cbf4e01bd4c44875a26bbe46
Jose Morrone <josemmorrone@gmail.com>
2022-03-21 13:08:09 -0400

    [UPDATE] Resuelto el nav-bar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -7,44 +7,48 @@ function Navbar() {
 				<a className="navbar-brand text-white ps-5" href="#">
 					Start Bootstrap
 				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavDropdown"
-					aria-controls="navbarNavDropdown"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div
-					className="collapse navbar-collapse"
-					id="navbarNavDropdown">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<a
-								className="nav-link active text-white"
-								aria-current="page"
-								href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link text-white" href="#">
-								About
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link text-white" href="#">
-								Service
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link text-white" href="#">
-								Contact
-							</a>
-						</li>
-					</ul>
+				<div>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarNavDropdown">
+						<div className="d-flex justify-content-end">
+							<ul className="navbar-nav">
+								<li className="nav-item">
+									<a
+										className="nav-link active text-white"
+										aria-current="page"
+										href="#">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										About
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										Service
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
